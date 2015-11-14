b272e8a6d2a5252855a52f9fc63dd8f3c69ae81b
sathish.govindaraju <sathish.govindaraju@autotrader.co.uk>
2015-11-14 03:58:14 +0000

    Minor refactor

--- src/main/PriceCalculator.java
+++ src/main/PriceCalculator.java
@@ -1,15 +1,14 @@
 package main;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class PriceCalculator {
-    private Map<Item, Integer> itemPrices;
+    private Map<Item, Integer> normalPrices;
     private Map<Item, Discount> discountPrices;
     private int total = 0;
 
-    public PriceCalculator(Map<Item, Integer> itemPrices, Map<Item, Discount> discountPrices) {
-        this.itemPrices = itemPrices;
+    public PriceCalculator(Map<Item, Integer> normalPrices, Map<Item, Discount> discountPrices) {
+        this.normalPrices = normalPrices;
         this.discountPrices = discountPrices;
     }
 
@@ -17,18 +16,29 @@ public class PriceCalculator {
 
         for (Item item : items.keySet()) {
             final boolean itemHasDiscount = hasDiscountForItem(item);
-            final Integer normalItemPrice = itemPrices.get(item);
+            final Integer normalItemPrice = normalPrices.get(item);
             final int numberOfItem = items.get(item);
             if(itemHasDiscount){
                 final int itemThreshold = getItemThresholdForDiscount(item);
                 final int discountPrice = getItemDiscountPrice(item);
-                total += ((numberOfItem % itemThreshold) * normalItemPrice) + (((numberOfItem - (numberOfItem % itemThreshold))/ itemThreshold) * discountPrice);
+                total = total +
+                        calculateNormalPrice(normalItemPrice, numberOfItem, itemThreshold) +
+                        calculateDiscountPrice(numberOfItem, itemThreshold, discountPrice);
             }else {
-                total += normalItemPrice * numberOfItem;
+                total = total +
+                        (normalItemPrice * numberOfItem);
             }
         }
 
         return total;
+    }
+
+    private int calculateDiscountPrice(int numberOfItem, int itemThreshold, int discountPrice) {
+        return ((numberOfItem - (numberOfItem % itemThreshold))/ itemThreshold) * discountPrice;
+    }
+
+    private int calculateNormalPrice(Integer normalItemPrice, int numberOfItem, int itemThreshold) {
+        return (numberOfItem % itemThreshold) * normalItemPrice;
     }
 
     private int getItemDiscountPrice(Item item) {
